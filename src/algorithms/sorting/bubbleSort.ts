b5a30c2c05e39a5509c22c6c8ecae6d7097f10ff
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2025-04-29 17:24:47 +0300

    add bubble sort

---
+++ src/algorithms/sorting/bubbleSort.ts
@@ -0,0 +1,20 @@
+export const bubbleSort = <T>(initialArr: T[]) => {
+    const arr = [...initialArr];
+
+    let swapped: boolean;
+
+    for (let i = 0; i < arr.length; i++) {
+        swapped = false;
+
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapped = true;
+            }
+        }
+
+        if (!swapped) break;
+    }
+
+    return arr;
+};
